ec21d43a4d5c20641cda2a0bb6e58dbb2c0e2c05
reneang17 <reneang17@gmail.com>
2026-02-20 09:43:05 -0600

    Fixed changing image

--- js/pic_switcher.js
+++ js/pic_switcher.js
@@ -2,13 +2,7 @@ function setHeaderImage() {
   const headerImage = document.getElementById("headerImage");
   const currentHour = new Date().getHours();
 
-  if (currentHour >= 6 && currentHour < 12) {
     headerImage.src = "assets/pexels-soumenmaity-634770_top.jpg"; // Replace with your morning image path
-  } else if (currentHour >= 19) {
-    headerImage.src = "assets/pexels-photo-239107.jpeg";
-  } else {
-    headerImage.src = "assets/pexels-soumenmaity-634770_top.jpg"; // Replace with your evening image path
-  }
 }
 
 // Call the function when the page loads
